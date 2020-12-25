70630954ea7b4fdbe399343c27361474e25179b7
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-12-25 11:09:54 -0500

    Graceful shutdown the http server

--- web/server.go
+++ web/server.go
@@ -2,10 +2,20 @@
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
+// The graceful-shutdown codes is based on the gin-gonic example codes
+// (graceful-shutdown by Bo-Yi Wu (appleboy)) that can be found
+// at https://github.com/gin-gonic/examples
+
 package web
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,15 +24,51 @@ func StartServer() {
 	port := "8080"
 
 	// Starts a new Gin instance with no middle-ware
-	r := gin.New()
+	router := gin.New()
 
 	// Define handlers
-	r.GET("/", func (c *gin.Context) {
+	router.GET("/", func (c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "TODO",
 		})
 	})
 
 	// Listen and serve on defined port
-	r.Run(":" + port)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Println("Server started")
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer func() {
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:%+v", err)
+	}
+
+	log.Println("Server exiting")
 }
