1490cf542fdf31dfee1abbf1ba45890bbfb0d275
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-12-26 09:16:45 -0500

    Add network scripts for docker deployment

---
+++ _deploy/docker/networks/setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0) && source ../.env
+
+
+NETWORK_NAMES=(${BACKEND_NETWORK_NAME} ${FRONTEND_NETWORK_NAME})
+
+for NETWORK_NAME in ${NETWORK_NAMES[@]}; do
+
+  if ! docker network ls | grep -q "${NETWORK_NAME}"; then
+    docker network create -d bridge ${NETWORK_NAME}
+  fi
+
+done
