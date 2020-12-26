f15d772945a339da4e1d0c41320ebd33cd46a4a3
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-12-26 10:05:10 -0500

    Add scripts to run and stop the app docker container

---
+++ _deploy/docker/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0) && source ./.env && source ../.env
+
+
+if ! docker ps | grep -q "${CONTAINER_NAME}"; then
+  if docker ps -a | grep -q "${CONTAINER_NAME}"; then
+    docker start ${CONTAINER_NAME}
+  else
+    ./networks/setup.sh
+
+    docker run -d \
+      --name ${CONTAINER_NAME} \
+      --network=${BACKEND_NETWORK_NAME} --hostname=${CONTAINER_NAME} \
+      -p ${PORT}:${PORT} \
+      ${IMAGE_FULL_NAME}
+  fi
+fi
