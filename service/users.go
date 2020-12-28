01542a53e4dfadddc6b34e1ec240b373202d7d48
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-12-28 08:18:45 -0500

    Add the user service using user model to provide user services

---
+++ service/users.go
@@ -0,0 +1,15 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package service
+
+import (
+	"github.com/chinmobi/gin-mvc/model"
+)
+
+// NOTE: The user service is just for demo, could to be removed for real project.
+
+type UserService struct {
+	userModel  model.UserModel
+}
