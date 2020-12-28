3408a557754d34dd68f168d10aa9a4382faaf388
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-12-28 07:19:13 -0500

    Add the ErrWrapErrors

---
+++ errors/wrapper.go
@@ -0,0 +1,48 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package errors
+
+import (
+	"strings"
+)
+
+type ErrWrapErrors struct {
+	errors []error
+}
+
+func NewErrWrapErrors() *ErrWrapErrors {
+	err := &ErrWrapErrors{
+		errors: make([]error, 0, 2),
+	}
+	return err
+}
+
+func (w *ErrWrapErrors) Wrap(err ...error) *ErrWrapErrors {
+	w.errors = append(w.errors, err...)
+	return w
+}
+
+func (w *ErrWrapErrors) WrappedErrors() []error {
+	return w.errors
+}
+
+func (w *ErrWrapErrors) Error() string {
+	var str strings.Builder
+
+	str.WriteString("errors: [")
+
+	for i, cnt := 0, len(w.errors); i < cnt; i++ {
+		if i > 0 {
+			str.WriteString(", ")
+		}
+		str.WriteString("[")
+		str.WriteString(w.errors[i].Error())
+		str.WriteString("]")
+	}
+
+	str.WriteString("]")
+
+	return str.String()
+}
