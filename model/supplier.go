cf5562714a548a2404a26a9b6dc6e05e8feebce8
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-12-27 19:16:17 -0500

    Add the models' supplier

---
+++ model/supplier.go
@@ -0,0 +1,30 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package model
+
+// The model supplier provides all of the models that will be used for services.
+type Supplier interface {
+	GetUserModel() UserModel
+}
+
+// The implementation of the model supplier.
+type SupplierSet struct {
+	userModel UserModel
+}
+
+func NewSupplierSet() *SupplierSet {
+	set := &SupplierSet{
+	}
+	return set
+}
+
+func (set *SupplierSet) SetUserModel(u UserModel) *SupplierSet {
+	set.userModel = u
+	return set
+}
+
+func (set *SupplierSet) GetUserModel() UserModel {
+	return set.userModel
+}
