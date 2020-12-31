491db277b187ddfbb0dec92bb400a6eca9704a02
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-12-30 22:16:57 -0500

    Add the security access evaluators

---
+++ security/access/evaluator.go
@@ -0,0 +1,72 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package access
+
+import (
+	"github.com/chinmobi/gin-mvc/security"
+)
+
+type PermissionEvaluator interface {
+	HasPermission(authentication security.Authentication, permission NeedPermission) bool
+}
+
+func SimplePermissionEval(authentication security.Authentication, permission NeedPermission) bool {
+	authorities := authentication.GetAuthorities()
+	size := len(authorities)
+	if size == 0 {
+		return false
+	}
+
+	permStr := permission.GetPermission()
+	for i := 0; i < size; i++ {
+		if authorities[i].GetAuthority() == permStr {
+			return true
+		}
+	}
+
+	return false
+}
+
+type PrivilegeEvaluator interface {
+	IsAllowed(authentication security.Authentication, permissions *PermissionsGroup) bool
+}
+
+func SimplePrivilegeEval(authentication security.Authentication, permissions *PermissionsGroup) bool {
+	authorities := authentication.GetAuthorities()
+	authSize := len(authorities)
+
+	needPermissions := permissions.NeedPermissions()
+	permSize := len(needPermissions)
+
+	if authSize == 0 {
+		if permSize == 0 {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	if permSize == 0 {
+		return true
+	}
+
+	for i := 0; i < permSize; i++ {
+		permStr := needPermissions[i].GetPermission()
+
+		for j := 0; j < authSize; j++ {
+			if authorities[j].GetAuthority() == permStr {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+type SimplePrivilegeEvaluator struct{}
+
+func (s SimplePrivilegeEvaluator) IsAllowed(auth security.Authentication, perms *PermissionsGroup) bool {
+	return SimplePrivilegeEval(auth, perms)
+}
