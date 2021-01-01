b2c4089c1aaeb47086023901c609909c540699a5
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-01 12:21:03 -0500

    Add the auth httpbasic and jwt plan

--- middleware/auth/config.go
+++ middleware/auth/config.go
@@ -6,6 +6,8 @@ package auth
 
 import (
 	"github.com/chinmobi/gin-mvc/app"
+	"github.com/chinmobi/gin-mvc/middleware/auth/httpbasic"
+	"github.com/chinmobi/gin-mvc/middleware/auth/jwt"
 	"github.com/chinmobi/gin-mvc/middleware/mw"
 	"github.com/chinmobi/gin-mvc/security/auth"
 )
@@ -13,9 +15,20 @@ import (
 func Configure(builder *mw.Builder, authConfig *auth.ProcessorConfigurer, app *app.App) error {
 	setUp(authConfig)
 
-	authGrop := auth.NewProcessorGroup(authConfig)
+	authGroup := auth.NewProcessorGroup(authConfig)
 
-	builder.AddMwAdapter(authGrop)
+	// Configure each of needed auth processors
+	// NOTE: The order of the processors is IMPORTANT!
+
+	if err := jwt.Configure(authGroup, app); err != nil {
+		return err
+	}
+
+	if err := httpbasic.Configure(authGroup, app); err != nil {
+		return err
+	}
+
+	builder.AddMwAdapter(authGroup)
 
 	return nil
 }
---
+++ middleware/auth/jwt/config.go
@@ -0,0 +1,14 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package jwt
+
+import (
+	"github.com/chinmobi/gin-mvc/app"
+	"github.com/chinmobi/gin-mvc/security/auth"
+)
+
+func Configure(authGroup *auth.ProcessorGroup, app *app.App) error {
+	return nil
+}
