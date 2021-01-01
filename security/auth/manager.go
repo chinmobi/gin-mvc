1579e5b2c41ec0a6b818377b289b274148a1fd58
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-01 08:27:09 -0500

    Add the AuthenticationManager

---
+++ security/auth/manager.go
@@ -0,0 +1,25 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package auth
+
+import (
+	"github.com/chinmobi/gin-mvc/security"
+	"github.com/chinmobi/gin-mvc/security/errors"
+)
+
+type ErrAuthentication = errors.ErrAuthentication
+
+// AuthenticationManager
+type AuthenticationManager interface {
+	Authenticate(authentication security.Authentication) (security.Authentication, error)
+}
+
+type AuthManager = AuthenticationManager
+
+// Wrap the errors.NewErrAuthentication
+
+func NewErrAuthentication(cause error) *ErrAuthentication {
+	return errors.NewErrAuthentication(cause)
+}
