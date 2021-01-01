605763107faf260380638e9499109bdd944d94dc
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-01 08:37:41 -0500

    Add the AuthenticationProcessorBase and ProcessorConfigurer

---
+++ security/auth/configurer.go
@@ -0,0 +1,61 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package auth
+
+import (
+	"github.com/chinmobi/gin-mvc/security"
+)
+
+// AuthenticationProcessorBase
+type AuthenticationProcessorBase struct {
+	manager      AuthManager
+	authHandler  security.AuthHandler
+}
+
+type AuthProcessorBase = AuthenticationProcessorBase
+
+func (ap *AuthProcessorBase) AuthManager() AuthManager {
+	return ap.manager
+}
+
+func (ap *AuthProcessorBase) AuthHandler() security.AuthHandler {
+	return ap.authHandler
+}
+
+// ProcessorConfigurer
+type ProcessorConfigurer struct {
+	processorBase  *AuthProcessorBase
+	manager        ProviderManager
+	handlerSet     security.AuthHandlerSet
+}
+
+func NewProcessorConfigurer() *ProcessorConfigurer {
+	configurer := &ProcessorConfigurer{
+	}
+	return configurer
+}
+
+func (pc *ProcessorConfigurer) ProcessorBase() *AuthProcessorBase {
+	if pc.processorBase != nil {
+		return pc.processorBase
+	}
+
+	processor := &AuthProcessorBase{
+		manager: &pc.manager,
+		authHandler: &pc.handlerSet,
+	}
+
+	pc.processorBase = processor
+
+	return processor
+}
+
+func (pc *ProcessorConfigurer) ProviderManager() *ProviderManager {
+	return &pc.manager
+}
+
+func (pc *ProcessorConfigurer) AuthHandlerSet() *security.AuthHandlerSet {
+	return &pc.handlerSet
+}
