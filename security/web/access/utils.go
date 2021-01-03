e0fad0b3aa50103f3e3a261a10a4252b4425326b
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-03 07:32:12 -0500

    Refactor: add the new access PermissionsConfigurer

---
+++ security/web/access/utils.go
@@ -0,0 +1,21 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package access
+
+import (
+	"github.com/chinmobi/gin-mvc/security/access"
+)
+
+func buildDefaultInterceptor(entry *PermissionsEntry) *SecurityInterceptor {
+	si := &SecurityInterceptor{
+		permissions:    &entry.permissions,
+		deniedHandler:  access.NullAccessDeniedHandler{},
+		evaluator:      access.SimplePrivilegeEvaluator{},
+	}
+
+	entry.interceptor = si
+
+	return si
+}
