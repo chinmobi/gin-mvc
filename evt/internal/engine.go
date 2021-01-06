cc7cb8fa0b02384e7cff421802ec815a2e69c616
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-06 18:11:28 -0500

    Implement the Event handle, bind and publish methods

--- evt/internal/engine.go
+++ evt/internal/engine.go
@@ -11,7 +11,10 @@ import (
 type Engine struct {
 	RouterGroup
 
+	multicaster    EventMulticaster
+
 	pool           sync.Pool
+	eventPool      sync.Pool
 
 	trees          topicTreeS
 	maxParams      uint16
@@ -19,7 +22,7 @@ type Engine struct {
 
 var _ IRouter = &Engine{}
 
-func NewEngine() *Engine {
+func NewEngine(multicaster EventMulticaster) *Engine {
 	engine := &Engine{
 		RouterGroup: RouterGroup{
 			Handlers:  nil,
@@ -27,19 +30,30 @@ func NewEngine() *Engine {
 			root:      true,
 		},
 	}
+
 	engine.RouterGroup.engine = engine
+
+	engine.multicaster = multicaster
 
 	engine.pool.New = func() interface{} {
 		return engine.allocateContext()
 	}
 
-	engine.trees = make(TopicTreeS, 0, 2)
+	engine.eventPool.New = func() interface{} {
+		return engine.allocateEvent()
+	}
+
+	engine.trees = make(topicTreeS, 0, 2)
 
 	return engine
 }
 
 func (engine *Engine) allocateContext() *Context {
-	return NewContext(engine.maxParams)
+	return newContext(engine.maxParams)
+}
+
+func (engine *Engine) allocateEvent() *Event {
+	return NewEvent()
 }
 
 // Use attaches a global middleware to the router. ie. the middleware attached though Use() will be
@@ -58,5 +72,48 @@ func (engine *Engine) addRoute(topic, path string, handlers HandlersChain) {
 	// Update maxParams
 	if paramsCount := countParams(path); paramsCount > engine.maxParams {
 		engine.maxParams = paramsCount
+	}
+}
+
+// --- Event handle, publish, ... ---
+
+func (engine *Engine) HandleEvent(event *Event) {
+	c := engine.pool.Get().(*Context)
+
+	engine.trees.handle(c, event)
+
+	engine.pool.Put(c)
+}
+
+func (engine *Engine) PublishEvent(topic, routingPath, source string, payload EventPayload) {
+	event := engine.eventPool.Get().(*Event)
+
+	event.Init(topic, routingPath, source, payload)
+	event.Handler = engine
+
+	engine.multicaster.MulticastEvent(event)
+}
+
+func (engine *Engine) ReplyEvent(event *Event, ack EventPayload) {
+	engine.PublishEvent(event.Topic, event.Source, event.RoutingPath, ack)
+}
+
+func (engine *Engine) BindEventListener(topic, bindingPath string, listener EventListener) {
+	handler := wrapListener(listener)
+
+	engine.RouterGroup.Register(topic, bindingPath, handler)
+}
+
+func wrapListener(listener EventListener) HandlerFunc {
+	return func(c * Context) {
+		listener.OnEvent(c, c.GetEventPayload())
+		recycleEvent(c)
+	}
+}
+
+func recycleEvent(c * Context) {
+	engine := c.getEngine()
+	if engine != nil {
+		engine.eventPool.Put(c.GetEvent().Reset())
 	}
 }
