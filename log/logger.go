0e09960a76de8f094f9c72af54739d931ecbd544
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-07 06:33:07 -0500

    Add convenient methods for log

--- log/logger.go
+++ log/logger.go
@@ -19,6 +19,54 @@ var (
 	SLogger *zap.SugaredLogger = zap.S()
 )
 
+func Debug(args ...interface{}) {
+	SLogger.Debug(args...)
+}
+
+func Debugf(template string, args ...interface{}) {
+	SLogger.Debugf(template, args...)
+}
+
+func Info(args ...interface{}) {
+	SLogger.Info(args...)
+}
+
+func Infof(template string, args ...interface{}) {
+	SLogger.Infof(template, args...)
+}
+
+func Warn(args ...interface{}) {
+	SLogger.Warn(args...)
+}
+
+func Warnf(template string, args ...interface{}) {
+	SLogger.Warnf(template, args...)
+}
+
+func Error(args ...interface{}) {
+	SLogger.Error(args...)
+}
+
+func Errorf(template string, args ...interface{}) {
+	SLogger.Errorf(template, args...)
+}
+
+func Panic(args ...interface{}) {
+	SLogger.Panic(args...)
+}
+
+func Panicf(template string, args ...interface{}) {
+	SLogger.Panicf(template, args...)
+}
+
+func Fatal(args ...interface{}) {
+	SLogger.Fatal(args...)
+}
+
+func Fatalf(template string, args ...interface{}) {
+	SLogger.Fatalf(template, args...)
+}
+
 func L() *zap.Logger {
 	return Logger
 }
