a92c91bdaca8e520ce51e91f192a2281f12340fc
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-06 22:52:45 -0500

    Refactor the default config

--- config/config.go
+++ config/config.go
@@ -5,53 +5,40 @@
 package config
 
 import (
-	"log"
 	"os"
-	"strconv"
 )
-
-const (
-	SERVER_PORT_ENV_NAME = "PORT"
-	DEFAULT_SERVER_PORT  = "8080"
-
-	INVALID_SERVER_PORT_LOG_FMT = "Invalid server port env: [%s], using the default value: [%s]\n"
-	EMPTY_SERVER_PORT_LOG_FMT   = "Null server port env [%s], using the default value: [%s]\n"
-)
-
-type Server struct {
-	Port string
-}
 
 type Config struct {
+	Logger Logger
 	Server Server
 	Grpool Grpool
 }
 
 func Default() *Config {
 	config := &Config{
+		Logger: Logger{
+			File: File{
+				Enabled: true,
+				Level: "INFO",
+				Filename: "/tmp/ginmvc.log", // "/var/log/ginmvc/ginmvc.log"
+				MaxSize: 500, // megabytes
+				MaxBackups: 3,
+				MaxAge: 28, // days
+				Compress: false,
+			},
+			Console: Console{
+				Enabled: true,
+				Level: "DEBUG",
+			},
+			Level: "INFO",
+		},
+		Server: Server{
+			Port: normalizePort(os.Getenv(SERVER_PORT_ENV_NAME)),
+		},
 		Grpool: Grpool{
 			Size: 8,
 		},
 	}
 
-	config.Server.Port = normalizePort(os.Getenv(SERVER_PORT_ENV_NAME))
-
 	return config
-}
-
-func normalizePort(port string) string {
-	if len(port) == 0 {
-		log.Printf(EMPTY_SERVER_PORT_LOG_FMT, SERVER_PORT_ENV_NAME, DEFAULT_SERVER_PORT)
-		return DEFAULT_SERVER_PORT
-	}
-
-	if i, err := strconv.Atoi(port); err == nil {
-		if i > 0 {
-			return port
-		}
-	}
-
-	log.Printf(INVALID_SERVER_PORT_LOG_FMT, port, DEFAULT_SERVER_PORT)
-
-	return DEFAULT_SERVER_PORT
 }
