be2d65bb9ac553242af5379273a38434ec0c6033
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-21 15:25:57 -0500

    Add scripts for RabbitMQ docker image

---
+++ _deploy/docker/rabbitmq/image/remove.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0) && source ../.env
+
+
+if docker images | grep -q "${IMAGE_REPOSITORY}"; then
+  docker image rm -f ${MANAGEMENT_IMAGE_FULL_NAME} && \
+  docker image rm -f ${IMAGE_FULL_NAME}
+fi
