bdd4d21ef229e7777680fd8750ed83a8c084e621
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-21 12:05:28 -0500

    Add script to create Redis cluster node containers

---
+++ _deploy/docker/redis/cluster/create.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0) && source ./NODE.variables && source ../.env
+
+
+if docker ps -a | grep -q "redis-${PORT_BEGIN}"; then
+  exit 0
+fi
+
+
+./prepare.sh
+../network-setup.sh
+
+
+for port in `seq $PORT_BEGIN $PORT_END`; do \
+  docker run -d \
+  --name redis-${port} \
+  -p ${port}:${port} -p 1${port}:1${port} \
+  -v $(pwd)/_deploy/${port}/conf/redis.conf:/usr/local/etc/redis/redis.conf \
+  -v $(pwd)/_deploy/${port}/data:/data \
+  --network=${BACKEND_NETWORK_NAME} --hostname=redis-${port} \
+  --sysctl net.core.somaxconn=1024 \
+  ${IMAGE_FULL_NAME} \
+  redis-server /usr/local/etc/redis/redis.conf; \
+done
+
+
+function get_container_ip() {
+  docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' redis-$1
+}
+
+
+REDIS_CLI_CMD="redis-cli --cluster create"
+
+for port in `seq $PORT_BEGIN $PORT_END`; do \
+  REDIS_CLI_CMD="${REDIS_CLI_CMD} $(get_container_ip ${port}):${port}"; \
+done
+
+REDIS_CLI_CMD="${REDIS_CLI_CMD} --cluster-replicas 1"
+
+
+docker exec -it redis-${PORT_BEGIN} \
+sh -c "echo '${REDIS_CLI_CMD}' && ${REDIS_CLI_CMD}"
