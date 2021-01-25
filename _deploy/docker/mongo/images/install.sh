50b81e88e08a93a4b78f5f37ccc835a2eec31d18
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-25 18:21:52 -0500

    Add the bitnami/mongodb-sharded docker image

--- _deploy/docker/mongo/images/install.sh
+++ _deploy/docker/mongo/images/install.sh
@@ -6,3 +6,7 @@ cd $(dirname $0) && source ../.env
 if ! docker images | grep -q "${IMAGE_REPOSITORY}"; then
   docker pull ${IMAGE_FULL_NAME}
 fi
+
+if ! docker images | grep -q "${SHARDED_IMAGE_REPOSITORY}"; then
+  docker pull ${SHARDED_IMAGE_FULL_NAME}
+fi
--- _deploy/docker/mongo/images/remove.sh
+++ _deploy/docker/mongo/images/remove.sh
@@ -3,6 +3,10 @@
 cd $(dirname $0) && source ../.env
 
 
+if docker images | grep -q "${SHARDED_IMAGE_REPOSITORY}"; then
+  docker image rm -f ${SHARDED_IMAGE_FULL_NAME}
+fi
+
 if docker images | grep -q "${IMAGE_REPOSITORY}"; then
   docker image rm -f ${IMAGE_FULL_NAME}
 fi
