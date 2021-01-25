b692aaaa42bd3505d657772cf9f49e5f933ff270
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-25 18:00:51 -0500

    Add scripts to run(start) and stop MongoDB replication nodes

---
+++ _deploy/docker/mongo/replicaset/run.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0) && source ./NODE.variables
+
+
+set_name=${REPLICA_SET_NAME}
+
+if [ "$#" -gt 0 ]; then
+  set_name=$1
+fi
+
+
+node_name="${set_name}-${NODE_BEGIN}"
+
+if docker ps | grep -q "${node_name}"; then
+  exit 0
+fi
+
+if ! docker ps -a | grep -q "${node_name}"; then
+  exit 1
+fi
+
+
+for node in `seq $NODE_BEGIN $NODE_END`; do \
+  node_name="${set_name}-${node}"; \
+  docker start ${node_name}; \
+done
+
+
+node_name="${set_name}-arbiter"
+
+if docker ps -a | grep -q "${node_name}"; then
+  docker start "${node_name}"
+fi
---
+++ _deploy/docker/mongo/replicaset/stop.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0) && source ./NODE.variables
+
+
+set_name=${REPLICA_SET_NAME}
+
+if [ "$#" -gt 0 ]; then
+  set_name=$1
+fi
+
+
+node_name="${set_name}-arbiter"
+
+if docker ps | grep -q "${node_name}"; then
+  docker stop "${node_name}"
+fi
+
+
+node_name="${set_name}-${NODE_BEGIN}"
+
+if ! docker ps | grep -q "${node_name}"; then
+  exit 0
+fi
+
+for node in `seq $NODE_END -1 $NODE_BEGIN`; do \
+  node_name="${set_name}-${node}"; \
+  docker stop ${node_name}; \
+done
