c0f44e580221000710c0219027b13656b2dd59fd
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-25 18:02:26 -0500

    Add script to clean MongoDB replication deployment

---
+++ _deploy/docker/mongo/replicaset/build/clean.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0) && source ../NODE.variables
+
+
+set_name=${REPLICA_SET_NAME}
+
+if [ "$#" -gt 0 ]; then
+  set_name=$1
+fi
+
+
+node_name="${set_name}-${NODE_BEGIN}"
+
+if docker ps | grep -q "${node_name}"; then
+  ../stop.sh
+fi
+
+
+if docker ps -a | grep -q "${node_name}"; then
+  for node in `seq $NODE_BEGIN $NODE_END`; do \
+    node_name="${set_name}-${node}"; \
+    docker container rm -f ${node_name}; \
+  done
+fi
+
+node_name="${set_name}-arbiter"
+
+if docker ps -a | grep -q "${node_name}"; then
+  docker container rm -f ${node_name}
+fi
+
+
+node_name="${set_name}-${NODE_BEGIN}"
+
+if [ ! -d "./${node_name}" ]; then
+  exit 0
+fi
+
+for node in `seq $NODE_BEGIN $NODE_END`; do \
+  node_name="${set_name}-${node}"; \
+  sudo rm -rf ./${node_name}; \
+done
